6bd6c1427533c4342362b37bb3d352033b4d2188
Hugo Gehl <gehlhugo@gmail.com>
2020-10-18 16:10:03 +0200

    base mvc

--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
-    echo "Hello lovers tv séries";
-?>
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config/debug.php';
+require_once __DIR__ . '/../config/db.php';
+require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../src/routing.php';
